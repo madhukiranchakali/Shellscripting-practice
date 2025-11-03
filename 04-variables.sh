acfe70c2cc85c965133e7eb8be477cc1bc3ecd30
madhukiran chakali <madhukiranel@gmail.com>
2025-11-03 18:19:17 +0530

    some message

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=1$
+PERSON2=2$
+
+echo "$PERSON1:: Welcome to Alska $PERSON2"
+echo "$PERSON2:: Thank You $PERSON!, Finally not on zoon webinar"
+echo "$PERSON1:: So, do we have a deal"
+echo "$PERSON2:: Yes, if you make ukraine joins Russia"
