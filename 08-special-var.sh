137437f0e5702cbf7c8692b24d65a4810913b9fe
madhukiran chakali <madhukiranel@gmail.com>
2025-11-04 00:19:32 +0530

    some message

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -2,3 +2,7 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
+echo "Script name: $o"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
