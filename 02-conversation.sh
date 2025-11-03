b3775049e5783232c1ea9e891ab30e1bf67e0294
madhukiran chakali <madhukiranel@gmail.com>
2025-11-03 12:15:56 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hi Ramesh, Iam fine. How are you?"
+echo "Ramesh:: What are you learning recently?"
+echo "Suresh:: I am learing shell scripting Ramesh"
