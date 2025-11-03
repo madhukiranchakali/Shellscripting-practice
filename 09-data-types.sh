a07635ba88772cf8ea6bb7c14ca90a169a2e8f73
madhukiran chakali <madhukiranel@gmail.com>
2025-11-04 01:20:28 +0530

    some message

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -7,3 +7,11 @@ NUMBER2=200
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM is: ${SUM}"
+
+#size=4, max Index=3
+
+LEADERS=("Modi" "Putin" "Turco" "Trump")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "First leader: ${LEADERS[0]}"
+echo "First leader: ${LEADERS[10]}"
