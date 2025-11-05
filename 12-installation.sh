0a483d9b91518ef2a325fbe205a166ed0181711f
madhukiran chakali <madhukiranel@gmail.com>
2025-11-06 02:07:59 +0530

    some message

---
+++ 12-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(is -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privileges"
+    exit 1
+fi
+
+dnf  install mysql -y
+
+if [$? -ne 0 ]: then
+    echo "ERROR:: Installating MySQL is FAILURE"
+    exit 1
+
+else 
+    echo "Installation MYSQL is SUCCESS"
+fi        
+
