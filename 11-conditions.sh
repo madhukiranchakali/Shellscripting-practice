8b6cc860045622b6f0723a59a5b1eabe7a46207f
madhukiran chakali <madhukiranel@gmail.com>
2025-11-06 01:20:16 +0530

    some message

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "please enter the number:"
-reah NUMBER
+read NUMBER
 
-if [ (($NUMBER % 2)) -eq 0 ] ; then 
+if [ $(($NUMBER % 2)) -eq 0 ] ; then 
     echo "Given number $NUMBER is EVEN"
 else
     echo "Given number $NUMBER is ODD"
