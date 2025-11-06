fbdadba0cbb9fbd17b09fc5067107216c650f7ff
madhukiran chakali <madhukiranel@gmail.com>
2025-11-06 12:46:23 +0530

    some message

--- 16-logs.sh
+++ 16-logs.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="var/log/Shellscripting-practice"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log
 
 mkdir -p $LOGS_FOLDER
+
 echo "Script started executed at: $date" 
 
-USERID=$(id -u)
 
-    if [ $USERID -ne 0 ]; then
-        echo "ERROR:: Please run this script with root privilege"
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privilege"
         exit 1
-    fi
+fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
@@ -33,7 +35,7 @@ dnf list installed mysql &>>$LOG_FILE
         dnf install mysql -y &>>$LOG_FILE
         VALIDATE $? "MYSQL"
     else
-        echo -e "MYSQL already exit....$Y SKIPPING $N" | tee -a $LOG_FILE
+        echo -e "MYSQL already exit....$Y SKIPPING $N"
     fi  
 
 dnf list installed nginx &>>$LOG_FILE
@@ -41,7 +43,7 @@ dnf list installed nginx &>>$LOG_FILE
         dnf install nginx -y &>>$LOG_FILE
         VALIDATE $? "Nginx"
     else
-        echo -e "Nginx already exist......$Y SKIPPING $N" | tee -a $LOG_FILE
+        echo -e "Nginx already exist......$Y SKIPPING $N"
     fi 
 
 dnf list installed python3 &>>$LOG_FILE
@@ -49,5 +51,5 @@ dnf list installed python3 &>>$LOG_FILE
         dnf install python3 -y &>>$LOG_FILE
         VALIDATE $? "Python3"
     else
-        echo -e "python3 already exit.....$Y SKIPPING $N" | tee -a $LOG_FILE
+        echo -e "python3 already exit.....$Y SKIPPING $N"
     fi           
