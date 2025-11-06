bd2179c1dc68043462c785b6892a3e2e45aa1fe5
madhukiran chakali <madhukiranel@gmail.com>
2025-11-06 08:47:21 +0530

    some message

---
+++ 13-installation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+    if [ USERID -ne 0 ]; then
+        echo "ERROR:: Please run this script with root privilege"
+        exit 1
+    fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installing $2 is failure"
+        exit
+    else
+        echo "Installating $2 is SUCCESS"
+    fi        
+}
+
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install python3 -y
+VALIDATE $? "python3"
