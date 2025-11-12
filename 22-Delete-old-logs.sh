47e629ff2a904e55d712408c8aad4d5938a23f8b
madhukiran chakali <madhukiranel@gmail.com>
2025-11-12 11:10:18 +0530

    some message

--- 22-Delete-old-logs.sh
+++ 22-Delete-old-logs.sh
@@ -16,7 +16,7 @@ echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 SOURCE_DIR=/home/ec2-user/app-logs
 
-if [ -d $SOURCE_DIR ]; then
+if [ ! -d $SOURCE_DIR ]; then
     echo -e "ERROR:: $SOURCE_DIR does not exist"
     exit 1
 fi
